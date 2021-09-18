cd9b28274c171def38bf8a1925080166b707ac68
Abhijeet Manny <abhijeetmanny@gmail.com>
2021-09-18 23:58:29 +0200

    Created CustomerData Form

---
+++ src/Form/CustomerDataType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CustomerData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomerDataType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('surname')
+            ->add('email_address')
+            ->add('telephone_number')
+            ->add('customer_type', ChoiceType::class, array('choices'=>['individual' => 'individual','company'=> 'company']))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => CustomerData::class,
+        ]);
+    }
+}
