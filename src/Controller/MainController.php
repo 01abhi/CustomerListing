bfb91ea1eabd2c3c4ec9cae91fe66f3d7de8240f
Abhijeet Manny <abhijeetmanny@gmail.com>
2021-09-19 02:56:38 +0200

    Add new customer data and form handling.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\CustomerData;
+use App\Form\CustomerDataType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -17,4 +21,31 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    /**
+     * @Route("/addcustomer", name="addcustomer")
+     */
+    public function addcustomer(Request $request){
+        $customerdata = new CustomerData();
+        $form = $this->createForm(CustomerDataType::class, $customerdata);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($customerdata);
+            $em->flush();
+
+            $this->addFlash(
+               'notice',
+               'Submitted Successfully'
+            );
+            return $this->redirectToRoute('main');
+        }
+        
+
+        return $this->render('main/addcustomer.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
 }
