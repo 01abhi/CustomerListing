ae7eb5eaf6e67096ea6f24216f7f3662a7389e6b
Abhijeet Manny <abhijeetmanny@gmail.com>
2021-09-19 02:17:03 +0200

    Fixed missing extension ChoiceType

--- src/Form/CustomerDataType.php
+++ src/Form/CustomerDataType.php
@@ -6,6 +6,7 @@ use App\Entity\CustomerData;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class CustomerDataType extends AbstractType
 {
